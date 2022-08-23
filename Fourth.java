85c5f5f10a77ac0f76aea20041a81b1dbf540698
YajneshVishwakarma <75421738+YajneshVishwakarma@users.noreply.github.com>
2022-08-23 09:24:50 +0530

    Add files via upload

---
+++ Fourth.java
@@ -0,0 +1,11 @@
+//Widening
+
+class Fourth
+{
+	public static void main(String[] args)
+	{
+		double num1 = (double) 10;
+		float num2 = 30;
+		System.out.println("\nint to double: "+num1+"\nint to float: "+num2);
+	}
+}
