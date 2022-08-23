6e27f776ee8e8f21c2ea4e48a1a11bdc16935087
YajneshVishwakarma <75421738+YajneshVishwakarma@users.noreply.github.com>
2022-08-23 09:05:49 +0530

    Add files via upload

---
+++ First.java
@@ -0,0 +1,24 @@
+import static java.lang.System.out;
+
+
+public class First {
+
+    public static void main(String[] args) {
+        out.println("My Details");
+        out.println("Name: Yajnesh");
+        out.println("Age: 21");
+        out.println("Qualification: B.E(Undergoing)");
+        out.println("Native: Manipal\n");
+
+        out.println("Family Details");
+        out.println("Father Name: Ramesh");
+        out.println("Mother Name: Shakunthala");
+        out.println("Sibling Name: Thanu");
+        out.println("Address: Manipal\n");
+
+        out.println("Friend Details");
+        out.println("Friend Name: Yash");
+        out.println("Friend's Crush Name: ");
+        out.println("Friend's Favorite Food: Chicken babbab");
+    }
+}
