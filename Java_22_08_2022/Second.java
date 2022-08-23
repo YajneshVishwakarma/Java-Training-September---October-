886d8dc457f906a7a46045158968b2c91c0e5086
YajneshVishwakarma <75421738+YajneshVishwakarma@users.noreply.github.com>
2022-08-23 15:19:09 +0530

    Add files via upload

---
+++ Java_22_08_2022/Second.java
@@ -0,0 +1,16 @@
+package Java_22_08_2022;
+
+class Second
+{
+	public static void main(String[] args)
+	{
+
+
+		String name = "amish";
+		int age = 22;
+
+		System.out.println(name);
+		System.out.println(age);
+	
+	}
+}
---
+++ Java_22_08_2022/Third.java
@@ -0,0 +1,16 @@
+package Java_22_08_2022;
+
+class Third
+{
+	public static void main(String[] args)
+	{
+
+
+		String name = "amish";
+		int age = 22;
+
+		System.out.println("Name: " + name);
+		System.out.println("Age: " + age);
+	
+	}
+}
