2454591b0a81162ce3ea8f0aa45ce0d27f7b4450
YajneshVishwakarma <75421738+YajneshVishwakarma@users.noreply.github.com>
2022-08-23 09:07:21 +0530

    Add files via upload

---
+++ Third.java
@@ -0,0 +1,14 @@
+class Third
+{
+	public static void main(String[] args)
+	{
+
+
+		String name = "amish";
+		int age = 22;
+
+		System.out.println("Name: " + name);
+		System.out.println("Age: " + age);
+	
+	}
+}
